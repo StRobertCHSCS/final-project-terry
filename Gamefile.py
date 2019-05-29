42f412572464ce264e2e1141e7f613c354d5543e
robuntu <robuntu@stro.ycdsb.ca>
2019-05-29 10:56:37 -0400

    ..

--- Gamefile.py
+++ Gamefile.py
@@ -1,5 +1,4 @@
 import arcade
-import time
 
 
 WIDTH = 1280
@@ -8,10 +7,11 @@ position_x = WIDTH/2
 position_y = HEIGHT/2
 
 up_pressed, down_pressed, right_pressed, left_pressed, fire = False, False, False, False, False
+player_idle_up, player_idle_down, player_idle_right, player_idle_left = False, False, False, False
 
 grid = []
 
-bullet_list_x, bullet_list_y = [], []
+bullet_list_x, bullet_list_y, bullet_direction = [], [], []
 bullet_count = 0
 bullet_index = []
 bullet_timer = 0
@@ -21,7 +21,7 @@ wall = arcade.load_texture("images/wall.png")
 
 
 def on_update(delta_time):
-    global position_x, position_y, bullet_count, bullet_index, bullet_timer
+    global position_x, position_y, bullet_direction, bullet_count, bullet_index, bullet_timer
 
     if up_pressed:
         position_y += 5
@@ -44,18 +44,37 @@ def on_update(delta_time):
     if fire and bullet_timer == 0:
         bullet_list_x.append(position_x)
         bullet_list_y.append(position_y)
+
+        if player_idle_up:
+            bullet_direction.append("up")
+        elif player_idle_down:
+            bullet_direction.append("down")
+        elif player_idle_right:
+            bullet_direction.append("right")
+        elif player_idle_left:
+            bullet_direction.append("left")
+        else:
+            bullet_direction.append("down")
+
         bullet_count += 1
         bullet_timer = 20
 
     for i in range(bullet_count):
-        bullet_list_y[i] += 10
+        if bullet_direction[i] == "up":
+            bullet_list_y[i] += 20
+        elif bullet_direction[i] == "down":
+            bullet_list_y[i] -= 20
+        elif bullet_direction[i] == "right":
+            bullet_list_x[i] += 20
+        elif bullet_direction[i] == "left":
+            bullet_list_x[i] -= 20
 
-        if bullet_list_y[i] >= 640:
+        if bullet_list_y[i] >= 640 or bullet_list_y[i] <= 80 or bullet_list_x[i] >= 1200 or bullet_list_x[i] <= 80:
             bullet_index.append(i)
-
     for j in bullet_index:
-        bullet_list_x.pop(j)
-        bullet_list_y.pop(j)
+        bullet_list_x.pop(0)
+        bullet_list_y.pop(0)
+        bullet_direction.pop(0)
         bullet_count -= 1
     bullet_index = []
 
@@ -89,35 +108,56 @@ def on_draw():
             """
 
     for i in range(bullet_count):
-        arcade.draw_rectangle_filled(bullet_list_x[i], bullet_list_y[i], 5, 10, arcade.color.YELLOW)
-    # arcade.draw_circle_filled(position_x, position_y, 25, arcade.color.BLUE)
-    arcade.draw_texture_rectangle(position_x, position_y, 60, 102, char_model)
+        arcade.draw_circle_filled(bullet_list_x[i], bullet_list_y[i], 5, arcade.color.YELLOW)
+    arcade.draw_circle_filled(position_x, position_y, 25, arcade.color.BLUE)
+    print(player_idle_down)
+    if player_idle_up:
+        arcade.draw_text("up", position_x, position_y, arcade.color.BLACK, 12)
+    elif player_idle_down:
+        arcade.draw_text("down", position_x, position_y, arcade.color.BLACK, 12)
+    elif player_idle_right:
+        arcade.draw_text("right", position_x, position_y, arcade.color.BLACK, 12)
+    elif player_idle_left:
+        arcade.draw_text("left", position_x, position_y, arcade.color.BLACK, 12)
+    else:
+        arcade.draw_text("down", position_x, position_y, arcade.color.BLACK, 12)
+
 
 def on_key_press(key, modifiers):
-    global up_pressed, down_pressed, right_pressed, left_pressed, fire
+    global up_pressed, down_pressed, right_pressed, left_pressed, fire, player_idle_up, player_idle_down, \
+        player_idle_right, player_idle_left
     if key == arcade.key.W:
         up_pressed = True
+        player_idle_up = True
     if key == arcade.key.S:
         down_pressed = True
+        player_idle_down = True
     if key == arcade.key.D:
         right_pressed = True
+        player_idle_right = True
     if key == arcade.key.A:
         left_pressed = True
+        player_idle_left = True
 
     if key == arcade.key.SPACE:
         fire = True
 
 
 def on_key_release(key, modifiers):
-    global up_pressed, down_pressed, right_pressed, left_pressed, fire
+    global up_pressed, down_pressed, right_pressed, left_pressed, fire, player_idle_up, player_idle_down, \
+        player_idle_right, player_idle_left
     if key == arcade.key.W:
         up_pressed = False
+        player_idle_up = False
     if key == arcade.key.S:
         down_pressed = False
+        player_idle_down = False
     if key == arcade.key.D:
         right_pressed = False
+        player_idle_right = False
     if key == arcade.key.A:
         left_pressed = False
+        player_idle_left = False
 
     if key == arcade.key.SPACE:
         fire = False
@@ -151,7 +191,8 @@ def setup():
             elif column == 0 or column == 15:
                 grid[row][column] = 1
 
-    print(grid)
+    grid[5][3] = 1
+
     arcade.run()
 
 
